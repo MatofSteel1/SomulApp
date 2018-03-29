b1bd61a9b5ac36087eefde9f9360d266170deb6b
MatofSteel1 <matthewjameshull123@gmail.com>
2018-03-29 16:13:20 -0700

    2 Sliders with Views and a Bluetooth Switch
    I uploaded the files from my local version of the project so I could get back into GitHub regularly. Making an app that will interface with a Bluetooth sensor on an Arduino.

---
+++ DeviceListActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Android.App;
+using Xamarin.Forms;
+
+namespace SomulApp
+{
+    [Activity(Label = "@string/select_device",
+                Theme = "@android:style/Theme.Holo.Dialog",
+                ConfigurationChanges = Android.Content.PM.ConfigChanges.KeyboardHidden | Android.Content.PM.ConfigChanges.Orientation)]
+
+    class DeviceListActivity
+    {
+        const string TAG = "DeviceListActivity";
+        public const string EXTRA_DEVICE_ADDRESS = "device_address";
+
+        BluetoothAdapter btAdapter;
+        static ArrayAdapter<string> newDevicesArrayAdapter;
+        DeviceDiscoveredReceiver receiver;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Setup the window
+            RequestWindowFeature(WindowFeatures.IndeterminateProgress);
+            SetContentView(Resource.Layout.activity_device_list);
+
+            // Set result CANCELED incase the user backs out
+            SetResult(Result.Canceled);
+
+            // Initialize the button to perform device discovery			
+            var scanButton = FindViewById<Button>(Resource.Id.button_scan);
+            scanButton.Click += (sender, e) =>
+            {
+                DoDiscovery();
+                (sender as View).Visibility = ViewStates.Gone;
+            };
+
+            // Initialize array adapters. One for already paired devices and
+            // one for newly discovered devices
+            var pairedDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+            newDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+
+            // Find and setup the ListView for paired devices
+            var pairedListView = FindViewById<ListView>(Resource.Id.paired_devices);
+            pairedListView.Adapter = pairedDevicesArrayAdapter;
+            pairedListView.ItemClick += DeviceListView_ItemClick;
+
+            // Find and set up the ListView for newly discovered devices
+            var newDevicesListView = FindViewById<ListView>(Resource.Id.new_devices);
+            newDevicesListView.Adapter = newDevicesArrayAdapter;
+            newDevicesListView.ItemClick += DeviceListView_ItemClick;
+
+            // Register for broadcasts when a device is discovered
+            receiver = new DeviceDiscoveredReceiver(this);
+            var filter = new IntentFilter(BluetoothDevice.ActionFound);
+            RegisterReceiver(receiver, filter);
+
+            // Register for broadcasts when discovery has finished
+            filter = new IntentFilter(BluetoothAdapter.ActionDiscoveryFinished);
+            RegisterReceiver(receiver, filter);
+
+            // Get the local Bluetooth adapter
+            btAdapter = BluetoothAdapter.DefaultAdapter;
+
+            // Get a set of currently paired devices
+            var pairedDevices = btAdapter.BondedDevices;
+
+            // If there are paired devices, add each on to the ArrayAdapter
+            if (pairedDevices.Count > 0)
+            {
+                FindViewById(Resource.Id.title_paired_devices).Visibility = ViewStates.Visible;
+                foreach (var device in pairedDevices)
+                {
+                    pairedDevicesArrayAdapter.Add(device.Name + "\n" + device.Address);
+                }
+            }
+            else
+            {
+                var noDevices = Resources.GetText(Resource.String.none_paired);
+                pairedDevicesArrayAdapter.Add(noDevices);
+            }
+        }
+}
---
+++ SomulApp/GreetingsPage.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SomulApp
+{
+    public class GreetingsPage : ContentPage
+    {
+        private Slider intensitySlider;
+        private Slider hueSlider;
+        private Label sliderLabel;
+        private ContentView hueView;
+
+        public GreetingsPage()
+        {
+            BackgroundColor = SomulColors.PrimaryDarker;
+
+            StackLayout intensitySliderLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+
+                Children = {
+
+                    (intensitySlider = new Slider(0.0, 100.0, 0.0)
+                    {
+                        Margin = new Thickness(10, 0, 0, 0),
+                        HorizontalOptions = LayoutOptions.FillAndExpand
+                    }),
+
+                    (sliderLabel = new Label
+                    {
+                        Margin = new Thickness(0, 0, 15, 0),
+                        Text = " Off",
+                        HorizontalTextAlignment = TextAlignment.Start,
+                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        TextColor = SomulColors.Accent
+                    })
+                }
+            };
+
+            StackLayout hueSliderLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+
+                Children = {
+
+                    (hueSlider = new Slider(0.0, 1.0, 0.0)
+                    {
+                        Margin = new Thickness(10, 0, 0, 0),
+                        HorizontalOptions = LayoutOptions.FillAndExpand
+                    }),
+
+                    (hueView  = new ContentView()
+                    {
+                         BackgroundColor = Color.Red,
+                         Margin = new Thickness(0, 0, 15, 0),
+                         HeightRequest = 30.0,
+                         WidthRequest = 30.0,
+                         HorizontalOptions = LayoutOptions.Center,
+                         VerticalOptions = LayoutOptions.Start
+                    })
+                }
+            };
+
+            StackLayout bluetoothIOLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+
+                Children = {
+
+                    new Label
+                    {
+                        Margin = new Thickness(20, 20, 20, 0),
+                        Text = "Bluetooth",
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        TextColor = SomulColors.Accent
+                    },
+
+                    new Switch
+                    {
+                        Margin = new Thickness(20, 20, 10, 0),
+                        IsToggled = false
+                    }
+                }
+            };
+
+            StackLayout bluetoothPickerLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+
+                Children = {
+                    new MenuItem
+                    {
+                        Text = "Bluetooth Menu"
+                    }
+                }
+            };
+
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+
+                Children = {
+
+                    new Label
+                    {
+                        Margin = new Thickness(20, 20 , 20, 0),
+                        Text = "Lamp Intensity",
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        TextColor = SomulColors.Accent
+                    },
+
+                    intensitySliderLayout,
+
+                    new Label
+                    {
+                        Margin = new Thickness(20, 10, 20, 0),
+                        Text = "Lamp Hue",
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        TextColor = SomulColors.Accent
+                    },
+
+                    hueSliderLayout,
+
+                    bluetoothIOLayout
+                }
+            };
+
+            intensitySlider.ValueChanged += IntensitySliderChanged;
+            hueSlider.ValueChanged += HueSliderChanged;
+
+        }
+
+        // Changes the color viewer's hue in real time
+        private void HueSliderChanged(object sender, ValueChangedEventArgs e)
+        {
+            hueView.BackgroundColor = Color.FromHsla(e.NewValue, 0.5, 0.5, 1.0);
+        }
+
+        // CANNOTDO: Set the initial slider to whatever the lamp's intensity is
+        // Snap the slider to 101 different discontinuous values (0-100%)
+        private void IntensitySliderChanged(object sender, ValueChangedEventArgs e)
+        {
+            intensitySlider.Value = Math.Round(e.NewValue, 0);
+            
+            // Display the percentage of intensity of the lamp
+            if ((intensitySlider.Value != 0) && (intensitySlider.Value != 100))
+            {
+                sliderLabel.Text = intensitySlider.Value.ToString() + " %";
+            }
+            // Display "Off" and "Max" for readablility
+            else if (intensitySlider.Value == 0)
+            {
+                sliderLabel.Text = " Off";
+            }
+            else
+            {
+                sliderLabel.Text = " Max";
+            }
+        }
+    }
+}
